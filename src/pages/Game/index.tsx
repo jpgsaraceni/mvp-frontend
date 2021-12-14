c41f77c540783aebd792ac9c875d366a0d51e7bc
Juliana Burzlaff <juliana.burzlaff@hotmail.com>
2021-12-14 15:24:51 -0300

    feat: add boards

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -3,6 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import GameTemplate from '../../components/GameTemplate';
 import points from '../../assets/images/points.svg';
 import exit from '../../assets/images/exit-button.svg';
+import firstBoard from '../../assets/images/first-board.svg';
+import secondBoard from '../../assets/images/second-board.svg';
+import thirdBoard from '../../assets/images/third-board.svg';
+
 import * as S from './styles';
 
 const Game = () => {
@@ -11,6 +15,11 @@ const Game = () => {
   return (
     <GameTemplate>
       <S.Points src={points} />
+
+      <S.FirstBoard src={firstBoard} />
+      <S.SecondBoard src={secondBoard} />
+      <S.ThirdBoard src={thirdBoard} />
+
       <S.Exit src={exit} onClick={() => navigateTo('/world')} />
     </GameTemplate>
   );
--- src/pages/Game/styles.ts
+++ src/pages/Game/styles.ts
@@ -7,6 +7,42 @@ export const Points = styled.img`
   left: 3%;
 `;
 
+export const FirstBoard = styled.img`
+  height: 206px;
+  position: absolute;
+  top: 20%;
+  left: 10%;
+  transition: all 0.7s ease;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const SecondBoard = styled.img`
+  height: 206px;
+  position: absolute;
+  top: 20%;
+  left: 37%;
+  transition: all 0.7s ease;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const ThirdBoard = styled.img`
+  height: 206px;
+  position: absolute;
+  top: 20%;
+  left: 64%;
+  transition: all 0.7s ease;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
 export const Exit = styled.img`
   height: 60px;
   position: absolute;
